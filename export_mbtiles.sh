2475b5f549dac5436c76ed4ccb36f44f3e84a9a7
Paulo Vieira <paulovieira@gmail.com>
2015-12-10 14:42:51 +0000

    update

--- export_mbtiles.sh
+++ export_mbtiles.sh
@@ -7,8 +7,21 @@ export TILEMILL_COMMAND="/home/pvieira/clima-app/tilemill-clima/index.js"
 
 project_name=()
 
-# find the name of the projects with "ls -ltra /home/pvieira/tilemill-files/project/"
-project_name+=("cirac-vul-bgri-fvi-n")
+# list all projects: "ls -ltra /home/pvieira/tilemill-files/project/cirac*"
+project_name+=("cirac_vul_cp4_bfvi")
+project_name+=("cirac_vul_cp4_bfvi75")
+project_name+=("cirac_vul_cp4_cfvi_75")
+project_name+=("cirac_vul_cp4_cfvi_mode")
+project_name+=("cirac_vul_cp4_e")
+project_name+=("cirac_vul_cp4_e75")
+project_name+=("cirac_vul_cp4_fvi")
+project_name+=("cirac_vul_cp4_fvi75")
+project_name+=("cirac_vul_cp4_sf")
+project_name+=("cirac_vul_cp4_sf75")
+project_name+=("cirac_vul_cp4_ssi")
+project_name+=("cirac_vul_cp4_ssi75")
+project_name+=("cirac_vul_cp4_tf")
+project_name+=("cirac_vul_cp4_tf75")
 
 l=${#project_name[@]}
 
@@ -20,7 +33,7 @@ for (( i=0; i < $l; i++ )) do
         $TILEMILL_FILES_DIR/export/${project_name[$i]}.mbtiles \
         --format=mbtiles \
         --minzoom=8 \
-        --maxzoom=13 \
+        --maxzoom=9 \
         --bbox="-9.5691,36.8928,-6.1194,42.2244" \
         --metatile=1 \
         --scale=1 \
