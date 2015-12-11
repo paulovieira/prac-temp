f21820cee5240cc0e0b381a8763ba4a207ad1146
paulovieira <paulovieira@gmail.com>
2015-12-11 19:53:07 +0000

    update

---
+++ create_tilejson.js
@@ -0,0 +1,58 @@
+//var Promise = require("bluebird");
+var Fs = require("fs");
+var Path = require("path");
+var Glob = require("glob");
+var ReadMultipleFiles = require("read-multiple-files");
+
+//var filesPaths = Glob.sync("tiles/cirac*.export");
+var filesPaths = Glob.sync("/home/pvieira/tilemill-files/project/cirac*/project.mml");
+
+var tileJsons = [];
+
+ReadMultipleFiles(filesPaths, function(err, files){
+
+	if(err){
+		throw new Error("error reading files")
+	}
+
+	files.forEach(function(file, i){
+
+		var tileJson = {};
+		//var mml = (JSON.parse(file.toString())).from.mml;
+		var mml = JSON.parse(file.toString());
+
+		//var filename = Path.basename(filesPaths[i], ".export");
+		var filename = filesPaths[i].split("/").splice(-2, 1).pop();
+
+		// reuse these properties from the mml file
+
+	    tileJson["bounds"] = mml["bounds"];
+	    tileJson["center"] = mml["center"];
+	    tileJson["minzoom"] = mml["minzoom"];
+	    tileJson["maxzoom"] = mml["maxzoom"];
+	    tileJson["name"] = mml["name"];
+	    tileJson["description"] = mml["description"];
+	    tileJson["legend"] = mml["legend"];
+
+	    // ad-hoc properties
+
+	    tileJson["id"] = filename;
+	    tileJson["isCirac"] = true;
+		tileJson["template"] = mml["interactivity"]["template_teaser"];
+		tileJson["tiles"] = ["/tiles/" + filename + "/{z}/{x}/{y}.png"];
+		tileJson["grids"] = ["/tiles/" + filename + "/{z}/{x}/{y}.grid.json"];
+
+		tileJsons.push(tileJson);
+	});
+
+	var output = "Cirac.layers = " + JSON.stringify(tileJsons, null, 4) + ";\n";
+
+	Fs.writeFile("./lib/web/client/cartografia-app/cirac-layers.js", output, function(err){
+
+		if(err){
+			throw err;
+		}
+
+		console.log("all done!");
+	});
+});
--- export_mbtiles.sh
+++ export_mbtiles.sh
@@ -2,26 +2,46 @@
 
 # automatic generate mbtiles for a bunch of map projects 
 
-export TILEMILL_FILES_DIR="/home/pvieira/tilemill-files"
 export TILEMILL_COMMAND="/home/pvieira/clima-app/tilemill-clima/index.js"
+export TILEMILL_FILES_DIR="/home/pvieira/tilemill-files"
+export OUTPUT_DIR="/home/pvieira/github/cirac/tiles"
+
 
 project_name=()
 
 # list all projects: "ls -ltra /home/pvieira/tilemill-files/project/cirac*"
-project_name+=("cirac_vul_cp4_bfvi")
-project_name+=("cirac_vul_cp4_bfvi75")
-project_name+=("cirac_vul_cp4_cfvi_75")
-project_name+=("cirac_vul_cp4_cfvi_mode")
-project_name+=("cirac_vul_cp4_e")
-project_name+=("cirac_vul_cp4_e75")
-project_name+=("cirac_vul_cp4_fvi")
-project_name+=("cirac_vul_cp4_fvi75")
-project_name+=("cirac_vul_cp4_sf")
-project_name+=("cirac_vul_cp4_sf75")
-project_name+=("cirac_vul_cp4_ssi")
-project_name+=("cirac_vul_cp4_ssi75")
-project_name+=("cirac_vul_cp4_tf")
-project_name+=("cirac_vul_cp4_tf75")
+# project_name+=("cirac_vul_cp4_bfvi")
+# project_name+=("cirac_vul_cp4_bfvi75")
+# project_name+=("cirac_vul_cp4_cfvi_75")
+# project_name+=("cirac_vul_cp4_cfvi_mode")
+# project_name+=("cirac_vul_cp4_e")
+# project_name+=("cirac_vul_cp4_e75")
+# project_name+=("cirac_vul_cp4_fvi")
+# project_name+=("cirac_vul_cp4_fvi75")
+# project_name+=("cirac_vul_cp4_sf")
+# project_name+=("cirac_vul_cp4_sf75")
+# project_name+=("cirac_vul_cp4_ssi")
+# project_name+=("cirac_vul_cp4_ssi75")
+# project_name+=("cirac_vul_cp4_tf")
+# project_name+=("cirac_vul_cp4_tf75")
+
+# project_name+=("cirac_vul_freg_sf")
+# project_name+=("cirac_vul_freg_sf75")
+# project_name+=("cirac_vul_freg_e75")
+# project_name+=("cirac_vul_freg_e")
+# project_name+=("cirac_vul_freg_tf75")
+# project_name+=("cirac_vul_freg_tf")
+# project_name+=("cirac_vul_freg_ssi75")
+# project_name+=("cirac_vul_freg_ssi")
+# project_name+=("cirac_vul_freg_bfvi75")
+# project_name+=("cirac_vul_freg_bfvi")
+# project_name+=("cirac_vul_freg_fvi75")
+# project_name+=("cirac_vul_freg_fvi")
+# project_name+=("cirac_vul_freg_cfvi_75")
+# project_name+=("cirac_vul_freg_cfvi_mode")
+
+project_name+=("cirac_vul_bgri_cfvi")
+project_name+=("cirac_vul_bgri_cfvi75")
 
 l=${#project_name[@]}
 
@@ -30,10 +50,10 @@ for (( i=0; i < $l; i++ )) do
     eval $(echo $TILEMILL_COMMAND \
         export \
         ${project_name[$i]} \
-        $TILEMILL_FILES_DIR/export/${project_name[$i]}.mbtiles \
+        $OUTPUT_DIR/${project_name[$i]}.mbtiles \
         --format=mbtiles \
-        --minzoom=8 \
-        --maxzoom=9 \
+        --minzoom=6 \
+        --maxzoom=15 \
         --bbox="-9.5691,36.8928,-6.1194,42.2244" \
         --metatile=1 \
         --scale=1 \
